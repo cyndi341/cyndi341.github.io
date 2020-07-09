a2a103b3c3fe535b09cc5eaaadb9dafc64b25571
cyndi341 <66556942+cyndi341@users.noreply.github.com>
2020-07-08 21:10:06 -0500

    Add files via upload

--- islt_7310/mail.php
+++ islt_7310/mail.php
@@ -1,10 +1,16 @@
-<?php $firstname = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "jenningsc@missouri.edu";
-$subject = "Professional Website Contact";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php 
+
+if (isset($_POST['submit'])) {
+ $name = $_POST['name']
+ $lastname = $_POST['lastname']
+ $mailFrom = $_POST['email']
+ $message = $_POST['message']
+
+ $mailTo = "jenningsc@missouri.edu";
+ $headers = "From: ".$mailFrom;
+ $txt = "You have received an e-mail from " .$name. ".\n\n". $message;
+
+ mail($mailTo, $txt, $headers);
+ header("Location: index.php?mailsend")
+}
 ?>
